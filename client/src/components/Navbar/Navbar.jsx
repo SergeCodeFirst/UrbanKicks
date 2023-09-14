0e5025ff91d36e48b2eaf3ee6ed2acc13e69fc3e
Itserge1 <sergecodefirst@gmail.com>
2023-09-14 16:44:14 -0400

    added smoyh scrolling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-scroll';
 import './Navbar.css'
 
 const Navbar = (props) =>{
@@ -17,11 +18,11 @@ const Navbar = (props) =>{
 
                 <div className={navOpen? "nav__menu show" : "nav__menu"} id="nav-menu">
                     <ul className="nav__list">
-                        <li className="nav__item"><a href="/" className="nav__link active">Home</a></li>
-                        <li className="nav__item"><a href="#featured" className="nav__link" onClick={toggleNavBar}>Featured</a></li>
-                        <li className="nav__item"><a href="#women" className="nav__link" onClick={toggleNavBar}>Women</a></li>
-                        <li className="nav__item"><a href="#new" className="nav__link" onClick={toggleNavBar} >New</a></li>
-                        <li className="nav__item"><a href="/shop" className="nav__link">Shop</a></li>
+                        <li className="nav__item"><a href="/" className="nav__link">Home</a></li>
+                        <li className="nav__item"><Link to="featured" smooth={true} spy={true} offset={-50} duration={500} className="nav__link" id="link" onClick={toggleNavBar}>Featured</Link></li>
+                        <li className="nav__item"><Link to="women" smooth={true} spy={true} offset={-50} duration={500} className="nav__link" id="link" onClick={toggleNavBar}>Women</Link></li>
+                        <li className="nav__item"><Link to="new" smooth={true} spy={true} offset={-50} duration={500} className="nav__link" id="link" onClick={toggleNavBar} >New</Link></li>
+                        <li className="nav__item"><a href="shop" className="nav__link">Shop</a></li>
                     </ul>
                 </div>
 
